f88da40ac559cf1b7df42653c8a0be91c94080e5
Flavio <flavio.danilo022@gmail.com>
2020-11-19 19:54:16 -0300

    OrderStatus enum

--- src/main/java/com/educandoweb/curso/config/TestConfig.java
+++ src/main/java/com/educandoweb/curso/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.educandoweb.curso.entities.Order;
 import com.educandoweb.curso.entities.User;
+import com.educandoweb.curso.entities.enums.OrderStatus;
 import com.educandoweb.curso.repositories.OrderRepository;
 import com.educandoweb.curso.repositories.UserRepository;
 
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner{
 		
 		User u1 = new User(null,"Flávio Costa","flavio.gordo@gmail.com","97747336","1234");
 		User u2 = new User(null,"Diego Sousa","diego.sousa@gmail.com","923232423","1234");
-		Order o1 = new Order(null, Instant.parse("2020-11-02T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2020-11-03T03:42:10Z"), u2);
-		
+		Order o1 = new Order(null, Instant.parse("2020-11-02T19:53:07Z"),OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2020-11-03T03:42:10Z"),OrderStatus.CANCELED, u2);
+
 		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2));
